5bf4dab3acf1294c24d653d920237e7718a675fc
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-07-20 20:18:02 -0300

    add operacao,  borda red em valicao incorreta

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const tagSection = document.getElementById('tester')
 
 let getPeso;
 let getAltura;
+let getOperacao;
 
 function pesoAltura(){
-    /* Validanco campos vazio */
-    if(peso.value !== "" && peso.value !== "0" && altura.value !== "" && altura.value !== "0"){
-        getPeso = peso.value
-        getAltura = altura.value
-    }
-    else{
+        getPeso = Number.parseInt(peso.value)
+        getAltura = Number.parseFloat(altura.value)
+}
+
+function validacao(){
+/* Validanco campos vazio */
+if(peso.value !== "" && peso.value !== "0" && altura.value !== "" && altura.value !== "0"){
+    pesoAltura()
+    operacao()
+}else{
         criaTag("É preciso que preencha os campos corretamente ")
     }
 }
@@ -21,11 +26,31 @@ function pesoAltura(){
 function criaTag(msmFinal){
     const tagP = document.createElement('p')
     tagP.textContent=`${msmFinal}`
+    tagP.style.color='red'
     tagSection.appendChild(tagP)
 }
+function operacao(){
+    getOperacao = getPeso / (getAltura * getAltura)
+    console.log(getOperacao.toFixed(1))
+}
+
+peso.addEventListener('blur', function(){
+    if(peso.value == "" || peso.value == 0){
+        peso.style.border = "1px solid red"
+    }else{
+        peso.style.border = ""
+    }
+})
+altura.addEventListener('blur', function(){
+    if(altura.value == "" || altura.value == 0){
+        altura.style.border = "1px solid red"
+    }else{
+        altura.style.border = ""
+    }
+})
 
 btnEnviar.addEventListener('click', function(e){
-    pesoAltura()
+    validacao()
 
 
     e.preventDefault()
